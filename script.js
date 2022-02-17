cc1611609e8d8d914b78be257057228bb4d99800
unknown <arsovmarko@gmail.com>
2022-02-17 14:54:15 +0100

    added random color change

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-var color = "green";
+
+var inter;
+
+function randomInt(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function randomColor(){
+    var r = randomInt(0,255);
+    var g = randomInt(0,255);
+    var b = randomInt(0,255);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
 function changeColor(){
     var div = document.getElementById("myDiv")
-    if (color === "green"){
-        div.style.backgroundColor = "red"
-        color = "red"
-        return
-    }
-    div.style.backgroundColor = "green"
-    color = "green"
-
+    var color = randomColor();
+    div.style.backgroundColor = color;
 }
