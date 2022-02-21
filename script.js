22325e9a39c91cfc0066e0ac0fbfe9fb99fedee4
unknown <arsovmarko@gmail.com>
2022-02-21 12:33:23 +0100

    better JS code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var inter;
 var counter = 0;
-var isCustom = false;
 
 var prevColor;
 var currColor;
@@ -9,31 +8,38 @@ function randomInt(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+var customColorInput
+
+var changeColorTypeButton
+var customColorButton
+var div
+var randomColorButton
+
 function loader() {
-    var div = document.getElementById("myDiv")
+    div = document.getElementById("myDiv")
     div.style.backgroundColor = randomColor();
     prevColor = div.style.backgroundColor;
 
-    var customColorInput = document.getElementById("customColorInput")
-    var customColorSubmit = document.getElementById("customColorSubmit")
-    var xButton = document.getElementById("customX")
-    var changeColorType = document.getElementById("changeColorType")
+    customColorInput = document.getElementById("customColorInput")
 
-    $(changeColorType).hide()
+
+    changeColorTypeButton = document.getElementById("changeColorType")
+    customColorButton = document.getElementById("customColorButton")
+    randomColorButton = document.getElementById("changeColorButton")
+
+    $(changeColorTypeButton).hide()
     $(customColorInput).hide()
-    $(customColorSubmit).hide();
-    $(xButton).hide();
+
+
 
 }
 
 function showPrevColor() {
-    var div = document.getElementById("myDiv")
     currColor = div.style.backgroundColor;
     div.style.backgroundColor = prevColor;
 }
 
 function showOriginalColor() {
-    var div = document.getElementById("myDiv")
     div.style.backgroundColor = currColor;
 }
 
@@ -45,8 +51,7 @@ function randomColor() {
 }
 
 function changeColor() {
-    var changeColorButton = document.getElementById("changeColorButton")
-    changeColorButton.disabled = true
+    randomColorButton.disabled = true
     counter = 0;
     prevColor = document.getElementById("myDiv").style.backgroundColor;
     inter = setInterval(intervalColor, 100);
@@ -54,57 +59,37 @@ function changeColor() {
 
 function intervalColor() {
     counter++;
-    var div = document.getElementById("myDiv")
     div.style.backgroundColor = randomColor();
     if (counter === 9) {
         clearInterval(inter);
-        var changeColorButton = document.getElementById("changeColorButton")
-        changeColorButton.disabled = false
+        randomColorButton.disabled = false
     }
 }
 
 function customColor() {
-    var customColorInput = document.getElementById("customColorInput")
-    var customColorSubmit = document.getElementById("customColorSubmit")
-    var customColorButton = document.getElementById("customColorButton")
-    var xButton = document.getElementById("customX")
-    var changeColorType = document.getElementById("changeColorType")
-
     $(customColorInput).show()
-    $(customColorSubmit).show();
-    $(xButton).show()
-    $(changeColorType).show();
-    $(customColorButton).hide()
+    $(changeColorTypeButton).show();
+    $(customColorButton).addClass("activeButton")
 }
 
 function submitCustomColor() {
-
-    var customColor = document.getElementById("customColorInput").value;
-    document.getElementById("customColorInput").value = "";
+    var customColor = customColorInput.value
+    customColorInput.value = "";
     customColor = customColor.toLowerCase().trim();
     if (customColor === "") return;
-    var div = document.getElementById("myDiv")
     prevColor = div.style.backgroundColor;
     div.style.backgroundColor = customColor;
 }
 
 function exitCustom() {
-    var customColorInput = document.getElementById("customColorInput")
-    var customColorSubmit = document.getElementById("customColorSubmit")
-    var customColorButton = document.getElementById("customColorButton")
-    var changeColorType = document.getElementById("changeColorType")
-    var xButton = document.getElementById("customX")
-
     $(customColorInput).hide()
-    $(customColorSubmit).hide()
-    $(xButton).hide()
-    $(changeColorType).hide();
+    $(changeColorTypeButton).hide();
     $(customColorButton).show()
 }
 
 var customColorInputType = true
+
 function changeColorType() {
-    var customColorInput = document.getElementById("customColorInput")
     if (customColorInputType) {
         customColorInput.type = "color"
         customColorInput.style.width = "240px"
